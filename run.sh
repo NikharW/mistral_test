49b6d9c4ec6a988d9a0d183e262d9bbda31a097f
NikharW <98466473+NikharW@users.noreply.github.com>
2025-02-09 00:24:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,11 @@ batch_size=(2 2 2 2)
 
 for i in {0..3}; do
   CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
-  
   wait
 
   git add --all
   git commit -m "Auto-update results from iteration $i"
-  git push https://NikharW:$GITHUB_TOKEN@github.com/NikharW/mistral_test.git main --force
+  git push https://github.com/NikharW/mistral_test.git main --force
 done
 
 echo "All evaluations completed and results pushed."
