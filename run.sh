8138e93eaa9d6fa7bf677aa642a82a3c2be00664
NikharW <98466473+NikharW@users.noreply.github.com>
2025-02-09 00:46:16 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,13 +2,17 @@
 
 batch_size=(250 250 500 1000)
 
+# Launch all evaluations in parallel
 for i in {0..3}; do
-  CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
-  wait
-
-  git add --all
-  git commit -m "Auto-update results from iteration $i"
-  git push https://github.com/NikharW/mistral_test.git main --force
+    (
+        CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh
+        git add --all
+        git commit -m "Auto-update results from GPU $i"
+        git push https://oauth2:${GITHUB_TOKEN}@github.com/NikharW/mistral_test.git main --force
+    ) &
 done
+
+# Wait for all background processes to complete
+wait
 
 echo "All evaluations completed and results pushed."
