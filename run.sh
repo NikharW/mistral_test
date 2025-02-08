61e37b35ae34403f6c3e6b3ad676c4e26a207a9d
NikharW <98466473+NikharW@users.noreply.github.com>
2025-02-09 00:50:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ for i in {0..3}; do
         CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh
         git add --all
         git commit -m "Auto-update results from GPU $i"
-        git push https://oauth2:${GITHUB_TOKEN}@github.com/NikharW/mistral_test.git main --force
+        git remote add origin_2 https://${GITHUB_TOKEN}@github.com/NikharW/mistral_test.git
     ) &
 done
 
