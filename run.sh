9caa3b43cd7d57c6483f1f2aa5dd5da53c088852
NikharW <98466473+NikharW@users.noreply.github.com>
2025-02-08 22:32:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# We have 8 GPUs available, but we need only 4 for the current batch sizes.
 batch_size=(250 250 500 1000)
 
 for i in {0..3}; do
   CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
+  
+  wait
+
+  git add --all
+  git commit -m "Auto-update results from iteration $i"
+  git push https://NikharW:$GITHUB_TOKEN@github.com/NikharW/mistral_test.git main --force
 done
 
-wait  # Wait for all background processes to finish
-
-echo "All evaluations completed."
+echo "All evaluations completed and results pushed."
