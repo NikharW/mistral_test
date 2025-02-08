848e30ff0e58be0e1b4a8c93fa38961bdcab9b18
NikharW <98466473+NikharW@users.noreply.github.com>
2025-02-08 22:34:49 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-batch_size=(250 250 500 1000)
+batch_size=(2 2 2 2)
 
 for i in {0..3}; do
   CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh &
