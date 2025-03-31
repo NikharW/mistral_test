2c94fc23eb8ec2de26fb9e0db540c58c23e25280
NikharW <98466473+NikharW@users.noreply.github.com>
2025-03-31 18:32:37 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@ batch_size=(250 250)
 
 # Launch all evaluations in parallel
 for i in {0..1}; do
-    (
-        export MASTER_ADDR=localhost
-        export MASTER_PORT=$((29500 + $i))
-        export RANK=$i
-        export WORLD_SIZE=2  # if you are running two processes in parallel    
+    (  
         CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh
     ) &
 done
