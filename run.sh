6f8d63c26737063defe19b0f4f3088f986367583
NikharW <98466473+NikharW@users.noreply.github.com>
2025-03-31 17:33:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-batch_size=(250)
+batch_size=(250 250 500 1000)
 
 # Launch all evaluations in parallel
-for i in {0}; do
+for i in {0..3}; do
     (
-        CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]}  ./evaluate_memorization.sh
+        CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh
     ) &
 done
-
 # Wait for all background processes to complete
 wait
-
-echo "All evaluations completed and results pushed."
