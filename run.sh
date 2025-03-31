7de3a718fd91f17b436bf3078c524fdc954be4d8
NikharW <98466473+NikharW@users.noreply.github.com>
2025-03-31 17:44:40 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-batch_size=(250 250 500 1000)
+batch_size=(250 250)
 
 # Launch all evaluations in parallel
-for i in {0..3}; do
+for i in {0..1}; do
     (
         CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i ./evaluate_memorization.sh
     ) &
