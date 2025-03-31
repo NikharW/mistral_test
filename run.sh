ff3637f29d6fda89f1473001fcc89efba9ee72bf
NikharW <98466473+NikharW@users.noreply.github.com>
2025-03-31 17:28:41 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# Set distributed environment variables for a single-process setup
-export MASTER_ADDR=localhost
-export MASTER_PORT=29500
-export RANK=0
-export WORLD_SIZE=1
-
-# Define fixed batch size in an array (only one value)
 batch_size=(250)
 
-# Launch evaluation process on GPU 0 (only one process)
+# Launch all evaluations in parallel
 for i in {0}; do
     (
-        CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} ./evaluate_memorization.sh
+        CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]}  ./evaluate_memorization.sh
     ) &
 done
 
-# Wait for the background process to complete
+# Wait for all background processes to complete
 wait
 
 echo "All evaluations completed and results pushed."
