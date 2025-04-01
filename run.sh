e956b7b041c81c502819312afe3431c56cfdc0f2
NikharW <98466473+NikharW@users.noreply.github.com>
2025-04-01 11:38:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 batch_size=(250 250)
 
 for i in {0..1}; do
-  CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$i+2 ./evaluate_memorization.sh &
+  CUDA_VISIBLE_DEVICES=$i BATCH_SIZE=${batch_size[$i]} MODEL_NUM=$((i+2)) ./evaluate_memorization.sh &
 done
 
 wait  # Wait for all background processes to finish
